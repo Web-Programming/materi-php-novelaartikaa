efc409ce3563fd20a6bfd6572419c01c1c04c2aa
novelaartikaa <novelaartikasaridevi@gmail.com>
2025-05-06 20:31:46 +0700

    commit: migration

--- Latihan/database/seeders/DatabaseSeeder.php
+++ Latihan/database/seeders/DatabaseSeeder.php
@@ -5,7 +5,8 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
+use DB;
+use Hash;
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -13,11 +14,17 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        //menggunakan query builder
+        // DB::table("users") -> insert([
+        //     'name' => 'Novela',
+        //     'email' => 'novelaartika@gmail.com',
+        //     'password' => Hash::make("password")
+        //]);
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        DB::table("users")
+        ->where("id",1)
+        ->update([
+            'password' => Hash::make("12345")
         ]);
     }
 }
