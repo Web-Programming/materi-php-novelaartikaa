c10ea100689b0989cd29b6ad36eec8c859be8829
novelaartikaa <novelaartikasaridevi@gmail.com>
2025-05-26 16:01:48 +0700

    commit: Membuat Authentikasi Final

--- Latihan/app/Http/Controllers/AdminController.php
+++ Latihan/app/Http/Controllers/AdminController.php
@@ -3,10 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Prodi;
 
 class AdminController extends Controller
 {
-    public function index() {
-        return view('home');
-    }
+    public function index()
+{
+    $listprodi = Prodi::all(); // ambil semua data prodi
+    return view('home', compact('listprodi')); // ✅ kirim ke view
+}
 }
--- Latihan/app/Http/Controllers/UserController.php
+++ Latihan/app/Http/Controllers/UserController.php
@@ -2,11 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Prodi;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    public function index() {
-        return view('home');
-    }
+    public function index()
+{
+    $listprodi = Prodi::all();
+    return view('home', compact('listprodi'));
+}
 }
